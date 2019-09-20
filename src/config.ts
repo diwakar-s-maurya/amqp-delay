a5b472e9bc8eeca29358f212446790c5626159ce
Diwakar Singh Maurya <diwakar.s.maurya@gmail.com>
2019-09-20 13:47:42 +0530

    fix: make AMQP server address configuration through env

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export default {
-    AMQP_CONNECTION_STRING: "amqp://user:password@localhost",
+    AMQP_CONNECTION_STRING: process.env.AMQP_CONNECTION_STRING || "amqp://user:password@localhost",
     DELAY_QUEUE_NAME: process.env.DELAY_QUEUE_NAME || "delay-queue",
     HEALTH_CHECK_HTTP_PORT: process.env.HEALTH_CHECK_HTTP_PORT || "3001",
 }
